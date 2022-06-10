5e635e3043d975591d87ad8aca170764313874be
Brian Wangora <brianwangora@gmail.com>
2022-06-10 17:44:59 +0300

    commit tree c7ee8eb304b0df31bdbd192664e9efb34c5f67bd

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 
-const reducer = (accumulator, item=Number()) => {
-    console.log(accumulator, item)
-    let totalBatteries =  item
-    return accumulator+= totalBatteries
+// const reducer = (accumulator, item=Number()) => {
+//     console.log(accumulator, item)
+//     let totalBatteries =  item
+//     return accumulator+= totalBatteries
+// }
+// let totalBatteries = batteryBatches.reduce(reducer, 0)
+
+function reducer (batteryBatches) {
+    let totalBatteries = 0
+    batteryBatches.reduce(function(i) {
+        totalBatteries = totalBatteries + 1
+    });
+    return totalBatteries
 }
-let totalBatteries = batteryBatches.reduce(reducer, 0)
